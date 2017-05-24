9652b551b001421fecdc0f4e8b502b695c9c93e6
EGOIST <0x142857@gmail.com>
2017-05-24 22:45:59 +0800

    Updates

---
+++ 5.58deeed6.js
@@ -0,0 +1,2 @@
+webpackJsonp([5],{47:function(e,o){e.exports={date:"2017-04-14T10:49:00.000Z",content:"<h2>book.page(page)</h2>\n<h3>page</h3>\n<h4>title</h4>\n<p>Type: <code>string</code><br>\nRequired: <code>true</code></p>\n<p>Chapter title</p>\n<h4>path</h4>\n<p>Type: <code>string</code><br>\nRequired: <code>true</code></p>\n<p>The path to add in router, eg: <code>/about</code></p>\n<h4>content</h4>\n<p>Type: <code>string</code> <code>object</code> <code>function</code></p>\n<p>Chapter content.</p>\n"}}});
+//# sourceMappingURL=5.58deeed6.js.map
